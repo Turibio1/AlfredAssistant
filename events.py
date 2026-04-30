1de251c966e60dc2e4ed3a5981d53be01f222f65
Roberto Turibio <robertoturibio2004@gmail.com>
2026-04-30 19:55:53 -0300

    Added onMemberJoinEvent

--- events.py
+++ events.py
@@ -22,3 +22,9 @@ async def setup_events(bot: commands.Bot):
             await message.channel.send(f'Olá, {message.author.name}!')
         
         await bot.process_commands(message)
+    
+    @bot.event
+    async def on_member_join(member):
+        channel = discord.utils.get(member.guild.text_channels, name='geral')
+        if channel:
+            await channel.send(f'Bem-vindo(a) ao servidor, {member.mention}!\n 🚨 ATENÇÃO🚨 \nQuem quiser adicionar algum amigo ou colega no servidor para estudar ou passar o tempo pode adicionar de boa, não tem problema ser alguém que a gente não conheça, é até bom pra fazer novas conexões, só não façam virar bagunça.\n O intuíto desse servidor é ser algo que a gente possa usar pra fazer qualquer coisa no discord, sem precisar ficar criando outros servidores e salas diferentes (porque o meu já virou uma bagunça).\n Deixei nós 6 como admins pra poder criar salas, mudar regras e etc. Depois eu crio cargos novos conforme as necessidades forem surgindo.')
